f57d9a25571272f2dfeafe3ce9f1561eec7e90a4
covjus01 <covjus01@gmail.com>
2025-03-29 17:40:29 -0400

    updated parse for all the diff columns(needs tidying)

--- src/parse.py
+++ src/parse.py
@@ -11,6 +11,8 @@ df = pd.read_csv(input_file)
 
 # Extract all unique elements from the FORMULA column
 element_counts = defaultdict(int)
+struct_counts = defaultdict(int)
+method_counts = defaultdict(int)
 formula_regex = re.compile(r"([A-Z][a-z]*)([\d\.]*)")
 
 for formula in df["FORMULA"].dropna():
@@ -18,12 +20,23 @@ for formula in df["FORMULA"].dropna():
     for element, _ in matches:
         element_counts[element] += 1
 
+for struct in df["PROPERTY: Microstructure"].dropna():
+    struct_counts[struct] += 1
+
+
+for method in df["PROPERTY: Processing method"].dropna():
+    method_counts[method] += 1
+
 # List of unique elements
 unique_elements = sorted(element_counts.keys())
+unique_structs = sorted(struct_counts.keys())
+unique_methods = sorted(method_counts.keys())
 
 # Create new columns for each element
 df = df.assign(**{element: 0.0 for element in unique_elements})
-df = df.assign(**{struct: 0.0 for struct in ["BCC", "FCC", "other"]})
+df = df.assign(**{struct: 0.0 for struct in unique_structs})
+df = df.assign(**{method: 0.0 for method in unique_methods})
+df = df.assign(**{struct: 0.0 for struct in ["BCC_col", "FCC_col", "other"]})
 df = df.assign(**{test: 0.0 for test in ["C_test", "T_test"]})
 
 # Populate the element columns
@@ -35,12 +48,19 @@ for idx, formula in df["FORMULA"].dropna().items():
     for element, value in element_values.items():
         df.at[idx, element] = value
 
+for idx, structure in df["PROPERTY: Microstructure"].dropna().items():
+        df.at[idx, structure] = 1.0
+
+
+for idx, method in df["PROPERTY: Processing method"].dropna().items():
+        df.at[idx, method] = 1.0
+
 # Encode Structure
 for idx, struct in df["PROPERTY: BCC/FCC/other"].dropna().items():
     if(struct == "BCC"):
-        df.at[idx, "BCC"] = 1.0
+        df.at[idx, "BCC_col"] = 1.0
     elif(struct == "FCC"):
-        df.at[idx, "FCC"] = 1.0
+        df.at[idx, "FCC_col"] = 1.0
     elif(struct == "other"):
         df.at[idx, "other"] = 1.0
 
