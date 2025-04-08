10e022b75bccec45b6e6d2c51278a21998edc341
KarlRizk <karl.rizk.nss@gmail.com>
2025-04-08 01:21:46 -0400

    Latest iteration. Some weird error where numpy can't square a float.

--- src/k_prototypes.py
+++ src/k_prototypes.py
@@ -12,15 +12,15 @@ dataset = pd.read_csv("../high-entropy-alloys-project/data/High_Entropy_Alloy_Pa
 fig_path = "../figures/ANN/"
 
 #Below is copied straight from ANN.py. Only difference is that unused_cols include the one-hot encoded categorical variables. 
+# X, the working array, should just be our desired categorical variables and numerical variables
 # Unused_cols really just drops every column that isnt the categorical variables or y variables; processing method and microstructure. 
 # Select the columns to be our inputs and our outputs
 # X =  Different output properties
 # Y =  Formula + Processing steps etc
-Unused_cols = [#"IDENTIFIER: Reference ID",
+Unused_cols = ["IDENTIFIER: Reference ID",
                "FORMULA",
                #"Processing method",
                # "Microstructure",
-               "BCC/FCC/other",
                "Type of test",
                "Ag",
                "B2",
@@ -57,12 +57,18 @@ Y_cols = ["grain size",
              "Other"]
 # This is an identifier for the datapoints. Used later on.
 syms = np.genfromtxt("../high-entropy-alloys-project/data/High_Entropy_Alloy_Parsed.csv", dtype=str, delimiter=',')[:, 0]
-# Here's where the error happens: KeyError: "['PROPERTY: BCC/FCC/other'] not found in axis"
-intermediate = dataset.drop(columns = Unused_cols)
-X = tools.preprocess(intermediate.to_numpy()[:, 1:])
 
+intermediate = dataset.drop(columns = Unused_cols)
+X = tools.preprocess(intermediate.to_numpy()[:, 2:]) # Pre-process data.
+"""
+Line gives the below error. 
+AttributeError: 'float' object has no attribute 'sqrt'
+
+The above exception was the direct cause of the following exception:
+"""
+X = np.concatenate((intermediate.to_numpy()[:, :1],X), axis=1) # Concantenate strings with pre-processed data. 
 # Replacing NaN with mean. Copied from https://stackoverflow.com/questions/18689235/numpy-array-replace-nan-values-with-average-of-columns
-#Obtain mean of columns as you need, nanmean is convenient.
+#Obtain mean of columns as you need, nanmean is convenient. Should be restricted to only numerical columns.
 col_mean = np.nanmean(X[:,2:], axis=0)
 
 #Find indices that you need to replace
